2ab1b4f1c4dc47852ea586bc25aa0f4286113e11
Macilquham <kronfield@hotmail.com>
2026-02-18 21:39:44 +1300

    first commit

---
+++ Mavis.BaseSixtyFour/BaseSixtyFourEncoder.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Text;
+
+namespace Mavis.BaseSixtyFour
+{
+    public class BaseSixtyFourEncoder
+    {
+        public string Encode(string stringToEncode)
+        {
+            //Convert.ToB
+            Byte[] bytes = new Byte[4];
+            var stream = new MemoryStream();
+            var byteArray = ASCIIEncoding.ASCII.GetBytes(stringToEncode);
+			int block =
+	(byteArray[0] << 16) |
+	(byteArray[1] << 8) |
+	byteArray[2];
+
+            int blocks =
+    (byteArray[0] << 16);
+			return "";
+        }
+    }
+}
---
+++ Mavis.BasetSixtyFour.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Mavis.BaseSixtyFour;
+
+namespace Mavis.BasetSixtyFour.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			var encoder = new BaseSixtyFourEncoder();
+			var encodedString = encoder.Encode("test");
+		}
+	}
+}
