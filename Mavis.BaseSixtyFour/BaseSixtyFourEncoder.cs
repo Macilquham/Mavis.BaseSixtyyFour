2ca0d0cafb92e5eb7d6b2e4541c15557f601fb00
Macilquham <kronfield@hotmail.com>
2026-02-19 23:12:30 +1300

    wired up encoder

--- Mavis.BaseSixtyFour/BaseSixtyFourEncoder.cs
+++ Mavis.BaseSixtyFour/BaseSixtyFourEncoder.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Text;
 
 namespace Mavis.BaseSixtyFour
 {
@@ -19,17 +21,35 @@ namespace Mavis.BaseSixtyFour
             
             var stream = new MemoryStream();
             var byteArray = ASCIIEncoding.ASCII.GetBytes(stringToEncode);
-			int block =	(byteArray[0] << 16) |	(byteArray[1] << 8) | byteArray[2];
+            var count = 0;
+            StringBuilder encodedText = new StringBuilder();
+            while(count + 3 <= byteArray.Length) {
+			    int block =	(byteArray[count] << 16) |	(byteArray[count + 1] << 8) | byteArray[count + 2];
+                encodedText.Append(base64Chars[block >> 18 & 0b111111]);
+                encodedText.Append(base64Chars[block >> 12 & 0b111111]);
+                encodedText.Append(base64Chars[block >> 6 & 0b111111]);
+                encodedText.Append(base64Chars[block & 0b111111]);
+                count = count + 3;
+            }
 
-            var block1 = base64Chars[block >> 18 & 0b111111];
-            var block2 = base64Chars[block >> 12 & 0b111111];
-            var block3 = base64Chars[block >> 6 & 0b111111];
-            var block4 = base64Chars[block & 0b111111];
+            if(byteArray.Length - count == 1)
+            {
+                var paddedTo12Bytes = byteArray[byteArray.Length - 1] << 4;
+                encodedText.Append(base64Chars[paddedTo12Bytes >> 6 & 0b111111]);
+                encodedText.Append(base64Chars[paddedTo12Bytes & 0b111111]);
+                encodedText.Append("==");
+            }
+            if(byteArray.Length - count == 2)
+            {
+                var paddedTo18Bytes = (byteArray[byteArray.Length - 2] << 8 | byteArray[byteArray.Length - 1]) << 2;
+                encodedText.Append(base64Chars[paddedTo18Bytes >> 12 & 0b111111]);
+                encodedText.Append(base64Chars[paddedTo18Bytes >> 6 & 0b111111]);
+                encodedText.Append(base64Chars[paddedTo18Bytes & 0b111111]);
+                encodedText.Append("=");
+            }
 
 
-
-
-			return "";
+			return encodedText.ToString();
         }
     }
 }
---
+++ Mavis.BasetSixtyFour.Tests/BaseSixtyFourEncoderTests.cs
@@ -0,0 +1,33 @@
+﻿using Mavis.BaseSixtyFour;
+using Shouldly;
+using System.Text;
+
+namespace Mavis.BasetSixtyFour.Tests
+{
+	public class BaseSixtyFourEncoderTests
+	{
+		[Fact]
+		public void ExactlyThreeBytes()
+		{
+			var encoder = new BaseSixtyFourEncoder();
+			var encodedString = encoder.Encode("tes");
+            encodedString.ShouldBe(Convert.ToBase64String(Encoding.ASCII.GetBytes("tes")));
+		}
+
+		[Fact]
+		public void OneByteExceess()
+		{
+			var encoder = new BaseSixtyFourEncoder();
+			var encodedString = encoder.Encode("test");
+            encodedString.ShouldBe(Convert.ToBase64String(Encoding.ASCII.GetBytes("test")));
+		}
+
+		[Fact]
+		public void TwoByteExceess()
+		{
+			var encoder = new BaseSixtyFourEncoder();
+			var encodedString = encoder.Encode("testj");
+            encodedString.ShouldBe(Convert.ToBase64String(Encoding.ASCII.GetBytes("testj")));
+		}
+	}
+}
